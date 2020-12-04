edfebcb12db90e24967d26330dc0919504c568fe
Thomas Chartron <thomas.chartron@gmail.com>
2020-12-04 20:44:32 +0100

    :rocket: Deploy to github pages script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+if [ -d "public" ]
+then
+    printf "\033[0;32mRemoving public folder except .git to keep the submodule...\033[0m\n"
+    cd public
+    find . -not -name '.git' -delete
+    cd ..
+fi
+
+printf "\033[0;32mDeploying updates to GitHub pages...\033[0m\n"
+
+# Build the project.
+# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hfng-tc
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+git commit -m "$msg"
+# Push source and build repos.
+git push origin main
